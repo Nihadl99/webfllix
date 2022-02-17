7cd9cb55b7ec4a93e69a5871aba645093ec429ff
Nihad Lazaar <nihadlazaar@gmail.com>
2022-02-17 10:37:19 +0100

    webflix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('synopsys');
+            $table->integer('duration');
+            $table->string('youtube')->nulltable();
+            $table->string('cover');
+            $table->timestamp('released_at')->nullable();
+            $table->foreignId('category_id')->nullable()->constrained();
+            $table->foreignId('user_id')->nulltable();
             $table->timestamps();
+            
         });
     }
 
